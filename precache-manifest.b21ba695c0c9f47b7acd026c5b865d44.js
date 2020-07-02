051cafc3949181976ace4307cfed7965ea123d53
a-Ksy <aaksoy17@ku.edu.tr>
2020-07-02 21:11:29 +0300

    Updates

---
+++ precache-manifest.b21ba695c0c9f47b7acd026c5b865d44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24e9e2cb31eca2c2673116691e69a31",
+    "url": "/laborodor/index.html"
+  },
+  {
+    "revision": "dd8bcb020a4213634a31",
+    "url": "/laborodor/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "d5c8cdf7fa6ba642f5ea",
+    "url": "/laborodor/static/css/main.50dd1ad0.chunk.css"
+  },
+  {
+    "revision": "dd8bcb020a4213634a31",
+    "url": "/laborodor/static/js/2.14f1c44d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/laborodor/static/js/2.14f1c44d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c8cdf7fa6ba642f5ea",
+    "url": "/laborodor/static/js/main.a3113b5a.chunk.js"
+  },
+  {
+    "revision": "21360c76c2cb450b2450",
+    "url": "/laborodor/static/js/runtime-main.60f4a6ed.js"
+  },
+  {
+    "revision": "9d62cb9fcefe1cb94d2d90553cbef3f1",
+    "url": "/laborodor/static/media/Proxima Nova Alt Bold.9d62cb9f.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/laborodor/static/media/ProximaNova-Regular.410504d4.otf"
+  }
+]);
